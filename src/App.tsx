71ac829ded2dc9ba2c53e60d581ee7f0e0346733
saeed221373 <saeed221373@googlemail.com>
2024-10-11 11:09:02 +0000

    add user data

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import baseLogo from '/base.svg'
 import './App.css'
+import WebApp from '@twa-dev/sdk'
+
+// Define the interface for user data
+interface UserData {
+  id: number;
+  first_name: string;
+  last_name?: string;
+  username?: string;
+  language_code: string;
+  is_premium?: boolean;
+}
 
 function App() {
   const [count, setCount] = useState(0)
+
+  const [userData, setUserData] = useState<UserData | null>(null)
+
+  useEffect(() => {
+    if (WebApp.initDataUnsafe.user) {
+      setUserData(WebApp.initDataUnsafe.user as UserData)
+    }
+  }, [])
 
   return (
     <>
       <div>
         <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
+          <img src={baseLogo} className="logo" alt="Vite logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+
+      <h1>Telegram Mini app</h1>
+      <main className="p-4">
+      {userData ? (
+        <>
+          <h1 className="text-2xl font-bold mb-4">User Data</h1>
+          <ul>
+            <li>ID: {userData.id}</li>
+            <li>First Name: {userData.first_name}</li>
+            <li>Last Name: {userData.last_name || 'N/A'}</li>
+            <li>Username: {userData.username || 'N/A'}</li>
+            <li>Language Code: {userData.language_code}</li>
+            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+          </ul>
+        </>
+      ) : (
+        <div>Loading...</div>
+      )}
+    </main>
+
+
+
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
