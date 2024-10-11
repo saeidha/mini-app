d9190cc5671498a6a4458e009ecc8d20e18a4969
saeed221373 <saeed221373@googlemail.com>
2024-10-11 13:48:21 +0000

    fix user interface

--- src/App.tsx
+++ src/App.tsx
@@ -26,45 +26,34 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={baseLogo} className="logo" alt="Vite logo" />
-        </a>
+
+      <h2>Telegram Mini app</h2>
+
+      <img src={baseLogo} className="logo" alt="Vite logo" onClick={() => setCount((count) => count + 1)} />
+
+      <div className="p-4">
+        {userData ? (
+          <>
+            <h4 className="text-2xl font-bold mb-4">User Data</h4>
+            <ul>
+              <li>ID: {userData.id}</li>
+              <li>First Name: {userData.first_name}</li>
+              <li>Last Name: {userData.last_name || 'N/A'}</li>
+              <li>Username: {userData.username || 'N/A'}</li>
+              <li>Language Code: {userData.language_code}</li>
+              <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
+            </ul>
+          </>
+        ) : (
+          <div>Loading...</div>
+        )}
       </div>
 
-      <h1>Telegram Mini app</h1>
-      <main className="p-4">
-      {userData ? (
-        <>
-          <h1 className="text-2xl font-bold mb-4">User Data</h1>
-          <ul>
-            <li>ID: {userData.id}</li>
-            <li>First Name: {userData.first_name}</li>
-            <li>Last Name: {userData.last_name || 'N/A'}</li>
-            <li>Username: {userData.username || 'N/A'}</li>
-            <li>Language Code: {userData.language_code}</li>
-            <li>Is Premium: {userData.is_premium ? 'Yes' : 'No'}</li>
-          </ul>
-        </>
-      ) : (
-        <div>Loading...</div>
-      )}
-    </main>
-
-
-
-      
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <button>the count is {count}</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <p className="read-the-docs">Tap on Base Icon</p>
+
     </>
   )
 }
